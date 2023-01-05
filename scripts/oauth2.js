ce6dfdad5a400b0cb2a97c2b4fe51bd8fcad8519
nature711 <e0665488@u.nus.edu>
2023-01-05 11:02:27 +0800

    setting up oauth2

--- scripts/oauth2.js
+++ scripts/oauth2.js
@@ -14,10 +14,11 @@ const oAuth2 = {
         this.init();
 
         let url = `${this.AUTHORIZATION_URL}?client_id=${this.CLIENT_ID}&redirect_uri=${this.REDIRECT_URL}&scope=${this.SCOPES[0]}`;
+        //https://github.com/login/oauth/authorize?client_id=79ff9c138224ad3504ba&redirect_uri=https://github.com/&scope=repo
 
-        await chrome.storage.local.set({pipe_my_leethub: true}); 
-        await chrome.tabs.create({url, active: true}); //open up redirect url in new tab
-        window.close();
-        await chrome.tabs.getCurrent((tab) => chrome.tabs.remove(tab.id)); //close current tab
+        await chrome.storage.local.set({my_pipe_leethub: true});
+        await chrome.tabs.create({url, active: true}); //open up redirect url in a new tab
+        //will trigger content script authorize.js to run (because content script is configured to run when url matches github.com/*)
+        //will check if there's a open pipe, and if so, issue a leethub token
     }
 }
