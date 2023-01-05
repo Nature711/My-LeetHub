2577cbbb03f7f2af7e0fbec6dc47ad4e600780d5
nature711 <e0665488@u.nus.edu>
2023-01-05 12:09:44 +0800

    link to onboarding page after authorization

--- scripts/background.js
+++ scripts/background.js
@@ -2,24 +2,18 @@ function handleMessage(request) {
   if (request && request.closeWebPage && request.isSuccess) {
     //these conditions are specified in the message body after successful authorization 
     /* Set username */
-    chrome.storage.local.set(
-      { leethub_username: request.username }, //the extracted github username
-      () => { window.localStorage.leethub_username = request.username; }
-    );
+    chrome.storage.local.set({ leethub_username: request.username }); //same as extracted github username
 
     /* Set token */
-    chrome.storage.local.set(
-        { leethub_token: request.token }, 
-        () => { window.localStorage[request.KEY] = request.token; }
-    );
+    chrome.storage.local.set({ leethub_token: request.token });
 
     /* Close pipe */
     chrome.storage.local.set(
         { pipe_leethub: false }, 
-        () => console.log('Closed pipe.')
+        () => console.log('Closed pipe. Going to onboarding page')
     );
 
-    chrome.tabs.getSelected(null, tab => chrome.tabs.remove(tab.id));
+    chrome.tabs.query({ active: true }, tabs => chrome.tabs.remove(tabs[0].id)); 
 
     /* Go to onboarding for UX */
     const urlOnboarding = chrome.runtime.getURL('welcome.html');
@@ -27,7 +21,7 @@ function handleMessage(request) {
 
   } else {
     alert('Something went wrong while trying to authenticate your profile!');
-    chrome.tabs.getSelected(null, tab => chrome.tabs.remove(tab.id));
+    chrome.tabs.query({ active: true }, tabs => chrome.tabs.remove(tabs[0].id)); 
   }
 }
 
